0a18543d7c1d3f40d6ec3e238032c0b3de3f9e8f
Shane <sfbreining@gmail.com>
2018-07-21 00:43:17 -0700

    Update 028 - Refactored the exec_cmd() to look a little cleaner. Added individual functions for each of the commands to do the validation checking before calling the functionality function. Cleans up the giant if-elif-else block in the exec_cmd().

--- k4thy/cmdmngr.py
+++ k4thy/cmdmngr.py
@@ -31,7 +31,7 @@ class CommandManager(Thread):
         self.lock = lock
 
         # Private attributes
-        self.__raffle_manager = rafflemngr.RaffleMngr(bot, bucket)
+        self.__raffle_manager__ = rafflemngr.RaffleMngr(bot, bucket)
         self.__command_queue = []
 
     def enqueue(self, cmd, e):
@@ -69,128 +69,54 @@ class CommandManager(Thread):
         """
 
         if cmd[0] == "game":
-            r = self.__get_streamer_info()
-            self.bot.send_message(r['display_name'] +
-                                  ' is currently playing ' +
-                                  r['game'])
+            self.__do_command_game__()
 
         # Poll the API to get current stream info
         elif cmd[0] == "title":
-            r = self.__get_streamer_info()
-            self.bot.send_message(r['display_name'] +
-                                  ' channel title is currently ' +
-                                  r['status'])
+            self.__do_command_title__()
 
         #
         # ----- Here begins point based commands -----
         #
-
         elif cmd[0] == "kernels":
-            self.bot.send_message("You have " + str(self.bucket.get_points(cmd[1].source.nick))
-                                  + " kernels", cmd[1].source.nick)
+            self.__do_command_kernels__(cmd[1])
 
         #
         # ----- Here begins mod commands -----
         #
-
         elif cmd[0] == "addcom":
-            if cmd[1].tags[0]['value'][:-2] != 'moderator'\
-                    and cmd[1].tags[0]['value'][:-2] != 'broadcaster':
-                self.__no_permission()
-                return
-            args = cmd[1].arguments[0].split('\"')
-            if len(args) != 3:
-                self.bot.send_message("Incorrect use of addcom")
-                return
-            commands = args[0].split(' ')
-            if self.bucket.add_command(commands[1], args[1]):
-                self.bot.send_message("Command was added successfully")
-            else:
-                self.bot.send_message("Command was NOT added, could already exist")
+            self.__do_command_addcom__(cmd[1])
 
         elif cmd[0] == "rmvcom":
-            if cmd[1].tags[0]['value'][:-2] != 'moderator'\
-                    and cmd[1].tags[0]['value'][:-2] != 'broadcaster':
-                self.__no_permission()
-                return
-            args = cmd[1].arguments[0].split(' ')
-            if len(args) != 2:
-                self.bot.send_message("Incorrect use of rmvcom")
-                return
-            if self.bucket.remove_command(args[1]):
-                self.bot.send_message("Command was removed successfully")
-            else:
-                self.bot.send_message("Command was NOT removed, might not exist?")
+            self.__do_command_rmvcom__(cmd[1])
 
         elif cmd[0] == "updatecom":
-            if cmd[1].tags[0]['value'][:-2] != 'moderator'\
-                    and cmd[1].tags[0]['value'][:-2] != 'broadcaster':
-                self.__no_permission()
-                return
-            args = cmd[1].arguments[0].split('\"')
-            if len(args) != 3:
-                self.bot.send_message("Incorrect use of updatecom")
-                return
-            commands = args[0].split(' ')
-            if self.bucket.update_command(commands[1], args[1]):
-                self.bot.send_message("Command was updated successfully")
-            else:
-                self.bot.send_message("Command didn't update, does it exist?")
+            self.__do_command_updatecom__(cmd[1])
 
         #
         # ----- Here beings raffle commands
         #
         elif cmd[0] == "beginraf":
-            if cmd[1].tags[0]['value'][:-2] == 'broadcaster':
-                mt, t = parse_flags(cmd[1].arguments[0])
-                if not cktools.is_integer(mt) or not cktools.is_integer(t):
-                    self.bot.send_message("The raffle needs whole number values for the options")
-                    return
-                self.__raffle_manager.set_options(mt, t)
-                self.__raffle_manager.start()
-            else:
-                return
+            self.__do_command_beginraf__(cmd[1])
 
         elif cmd[0] == "redraw":
-            if cmd[1].tags[0]['value'][:-2] == 'broadcaster':
-                self.__raffle_manager.redraw_winner()
-            else:
-                return
+            self.__do_command_redraw__(cmd[1])
 
         elif cmd[0] == "cancelraf":
-            if cmd[1].tags[0]['value'][:-2] == 'broadcaster':
-                self.__raffle_manager.interrupt()
+            self.__do_command_cancelraf__(cmd[1])
 
         elif cmd[0] == "closeraf":
-            if cmd[1].tags[0]['value'][:-2] == 'broadcaster':
-                self.__raffle_manager.manual_close_raffle()
+            self.__do_command_closeraf__(cmd[1])
 
-        elif cmd[0] == "here" and cmd[1].source.nick == self.__raffle_manager.get_winner():
-            self.__raffle_manager.winner_found()
+        elif cmd[0] == "here":
+            self.__do_command_here__(cmd[1])
 
         elif cmd[0] == "ticket":
-            args = cmd[1].arguments[0].split(' ')
-            user_total_tickets = self.bucket.get_points(cmd[1].source.nick)
-            try:
-                t = int(args[1])
-            except ValueError:
-                self.bot.send_message("To enter a drawing, you must enter with\
-                                      a whole number of tickets.", cmd[1].source.nick)
-                return
-            if t > user_total_tickets:
-                self.bot.send_message("You don't have that many tickets to submit. Your \
-                                       total tickets are " + str(user_total_tickets), cmd[1].source.nick)
-            elif t < 0:
-                self.bot.send_message("Don't be an idiot trying to submit less than \
-                                      0 tickets", cmd[1].source.nick)
-            else:
-                self.__raffle_manager.submit_tickets(cmd[1].source.nick, t)
-            return
+            self.__do_command_ticket__(cmd[1])
 
         #
         # ---- Here begins poker commands
         #
-
         elif cmd[0] == "poker":
             pass
 
@@ -201,9 +127,9 @@ class CommandManager(Thread):
             self.bot.send_message(self.bucket.get_command_response(cmd[0]))
 
     #
-    # ------ Here starts miscellaneous helper functions
+    # ------ Here starts miscellaneous helper functions and private functions
     #
-    def __no_permission(self):
+    def __no_permission__(self):
         """
         This just informs the user that they don't have permission to run the
         command they entered.
@@ -212,7 +138,7 @@ class CommandManager(Thread):
         """
         self.bot.send_message("You don't have permission to use that command")
 
-    def __get_streamer_info(self):
+    def __get_streamer_info__(self):
         """
         API call collecting the streamers info. A lot of info actually.
 
@@ -221,3 +147,111 @@ class CommandManager(Thread):
         url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
         headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
         return requests.get(url, headers=headers).json()
+
+    def __do_command_game__(self):
+        r = self.__get_streamer_info__()
+        self.bot.send_message(r['display_name'] +
+                              ' is currently playing ' +
+                              r['game'])
+
+    def __do_command_title__(self):
+        r = self.__get_streamer_info__()
+        self.bot.send_message(r['display_name'] +
+                              ' channel title is currently ' +
+                              r['status'])
+
+    def __do_command_kernels__(self, chatter):
+        self.bot.send_message("You have " + str(self.bucket.get_points(chatter.source.nick))
+                              + " kernels", chatter.source.nick)
+
+    def __do_command_addcom__(self, mod):
+        if mod.tags[0]['value'][:-2] != 'moderator' \
+                and mod.tags[0]['value'][:-2] != 'broadcaster':
+            self.__no_permission__()
+            return
+        args = mod.arguments[0].split('\"')
+        if len(args) != 3:
+            self.bot.send_message("Incorrect use of addcom")
+            return
+        commands = args[0].split(' ')
+        if self.bucket.add_command(commands[1], args[1]):
+            self.bot.send_message("Command was added successfully")
+        else:
+            self.bot.send_message("Command was NOT added, could already exist")
+
+    def __do_command_rmvcom__(self, mod):
+        if mod.tags[0]['value'][:-2] != 'moderator' \
+                and mod.tags[0]['value'][:-2] != 'broadcaster':
+            self.__no_permission__()
+            return
+        args = mod.arguments[0].split(' ')
+        if len(args) != 2:
+            self.bot.send_message("Incorrect use of rmvcom")
+            return
+        if self.bucket.remove_command(args[1]):
+            self.bot.send_message("Command was removed successfully")
+        else:
+            self.bot.send_message("Command was NOT removed, might not exist?")
+
+    def __do_command_updatecom__(self, mod):
+        if mod.tags[0]['value'][:-2] != 'moderator' \
+                and mod.tags[0]['value'][:-2] != 'broadcaster':
+            self.__no_permission__()
+            return
+        args = mod.arguments[0].split('\"')
+        if len(args) != 3:
+            self.bot.send_message("Incorrect use of updatecom")
+            return
+        commands = args[0].split(' ')
+        if self.bucket.update_command(commands[1], args[1]):
+            self.bot.send_message("Command was updated successfully")
+        else:
+            self.bot.send_message("Command didn't update, does it exist?")
+
+    def __do_command_beginraf__(self, broadcaster):
+        if broadcaster.tags[0]['value'][:-2] == 'broadcaster':
+            mt, t = parse_flags(broadcaster.arguments[0])
+            if not cktools.is_integer(mt) or not cktools.is_integer(t):
+                self.bot.send_message("The raffle needs whole number values for the options")
+                return
+            self.__raffle_manager__.set_options(mt, t)
+            self.__raffle_manager__.start()
+        else:
+            return
+
+    def __do_command_redraw__(self, broadcaster):
+        if broadcaster.tags[0]['value'][:-2] == 'broadcaster':
+            self.__raffle_manager__.redraw_winner()
+        else:
+            return
+
+    def __do_command_cancelraf__(self, broadcaster):
+        if broadcaster.tags[0]['value'][:-2] == 'broadcaster':
+            self.__raffle_manager__.interrupt()
+
+    def __do_command_closeraf__(self, broadcaster):
+        if broadcaster.tags[0]['value'][:-2] == 'broadcaster':
+            self.__raffle_manager__.manual_close_raffle()
+
+    def __do_command_here__(self, viewer):
+        if viewer.source.nick == self.__raffle_manager__.get_winner():
+            self.__raffle_manager__.winner_found()
+
+    def __do_command_ticket__(self, viewer):
+        args = viewer.arguments[0].split(' ')
+        user_total_tickets = self.bucket.get_points(viewer.source.nick)
+        try:
+            t = int(args[1])
+        except ValueError:
+            self.bot.send_message("To enter a drawing, you must enter with\
+                                              a whole number of tickets.", viewer.source.nick)
+            return
+        if t > user_total_tickets:
+            self.bot.send_message("You don't have that many tickets to submit. Your \
+                                               total tickets are " + str(user_total_tickets), viewer.source.nick)
+        elif t < 0:
+            self.bot.send_message("Don't be an idiot trying to submit less than \
+                                              0 tickets", viewer.source.nick)
+        else:
+            self.__raffle_manager__.submit_tickets(viewer.source.nick, t)
+        return
--- run_chatty.py
+++ run_chatty.py
@@ -1,25 +1,19 @@
+"""
+Initiator of the program. Needs command line arguments to get started.
+The arguments are needed at the command line because they are the secret
+token and client id needed from starting a developer account with Twitch.
+"""
+
 import sys
 from ch4tty.chatbot import TwitchBot
 
 
 def main():
-    """
-    Initiator of the program. Needs command line arguments to get started.
-    The arguments are needed at the command line because they are the secret
-    token and client id needed from starting a developer account with Twitch.
-
-    :return:
-    """
     if len(sys.argv) != 5:
         print("Usage: twitchbot <username> <client id> <token> <channel>")
         sys.exit(1)
 
-    username = sys.argv[1]
-    client_id = sys.argv[2]
-    token = sys.argv[3]
-    channel = sys.argv[4]
-
-    bot = TwitchBot(username, client_id, token, channel)
+    bot = TwitchBot(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     bot.start()
 
 
